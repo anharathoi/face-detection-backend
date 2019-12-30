224675577357c80f8987dbe7d7611719d82d3396
Anhar Athoi <anharathoi@admins-MacBook-Pro.local>
2019-12-30 11:01:22 +1100

    basic image upload functionality implemented-- all hard-coded at the moment

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const fs = require('fs');
+const AWS = require('aws-sdk')
 const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
 require('dotenv').config()
 
-const AWS = require('aws-sdk')
 const bucket = 'image-rekog-anhar' // the bucketname without s3://
 const photo  = 'beard.jpeg' // the name of file
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
 
 const config = new AWS.Config({
   accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -13,26 +24,46 @@ const config = new AWS.Config({
   region: process.env.AWS_REGION
 })
 
-const client = new AWS.Rekognition(config);
-const params = {
-  Image: {
-    S3Object: {
-      Bucket: bucket,
-      Name: photo
-    },
-  },
-  Attributes: ['ALL']
-}
+app.get('/', (req, res) => {
+  return res.send("App is working!")
+})
 
-app.get( '/', (req, res) => {
-  client.detectFaces(params, (err, response) => {
-    if (err) {
-      return res.send(err); // an error occurred
-    } else {
-      console.log(`Detected faces for: ${photo}`)
-      return res.send(response)
-    }
-  });
+app.get( '/rekog', (req, res) => {
+  // const client = new AWS.Rekognition(config);
+  // const params = {
+  //   Image: {
+  //     S3Object: {
+  //       Bucket: bucket,
+  //       Name: photo
+  //     },
+  //   },
+  //   Attributes: ['ALL']
+  // }
+
+  // client.detectFaces(params, (err, response) => {
+  //   if (err) {
+  //     return res.send(err); // an error occurred
+  //   } else {
+  //     console.log(`Detected faces for: ${photo}`)
+  //     return res.send(response)
+  //   }
+  // });
+})
+
+app.get('/upload', (req, res) => {
+  // const fileContent = fs.readFileSync('./2.jpg');
+  // const params = {
+  //   Bucket: 'image-rekog-anhar',
+  //   Key: '2.jpg',
+  //   Body: fileContent
+  // }
+
+  // s3.upload(params, (err, data) => {
+  //   if (err) {
+  //       throw err;
+  //   }
+  //   res.send(`File uploaded successfully. ${data.Location}`);
+  // });
 })
 
 app.listen( 3000, () => {
