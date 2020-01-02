dbf246c6c2890f1ae097aeba5aa70a4f230c8baa
Anhar Athoi <anharathoi@admins-MacBook-Pro.local>
2020-01-03 10:02:15 +1100

    Added eslint

--- server.js
+++ server.js
@@ -1,48 +1,45 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const multer = require('multer')
-const multerS3 = require('multer-s3')
-const fs = require('fs');
-const AWS = require('aws-sdk')
-const PORT = process.env.PORT || 3000
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
+// test
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
+require('dotenv').config();
 
-app.use(express.json())
-app.use(cors())
-require('dotenv').config()
-
-const bucket = 'image-rekog-anhar' // the bucketname without s3://
+const bucket = 'image-rekog-anhar';
 
 // for s3
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 const config = new AWS.Config({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
-})
+  region: process.env.AWS_REGION,
+});
 const client = new AWS.Rekognition(config);
 
-app.get('/', (req, res) => {
-  return res.json({"message": "App is working!"})
-})
+app.get('/', (req, res) => res.json({ message: 'App is working!' }));
 
 // s3 upload
-let upload = multer({
+const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: 'image-rekog-anhar',
-    metadata: function (req, file, cb) {
-      cb(null, {fieldName: file.fieldname});
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
     },
-    key: function (req, file, cb) {
-      cb(null, `${Date.now().toString()}${file.originalname}`)
-    }
-  })
-})
+    key: (req, file, cb) => {
+      cb(null, `${Date.now().toString()}${file.originalname}`);
+    },
+  }),
+});
 
 app.post('/upload', upload.single('image'), (req, res, next) => {
   const fileName = req.file.key
@@ -52,23 +49,22 @@ app.post('/upload', upload.single('image'), (req, res, next) => {
     Image: {
       S3Object: {
         Bucket: bucket,
-        Name: fileName
+        Name: fileName,
       },
     },
-    Attributes: ['ALL']
-  }
+    Attributes: ['ALL'],
+  };
+
   client.detectFaces(params, (err, response) => {
     if (err) {
       res.send(err); // an error occurred
     } else {
-      console.log("here");
-      console.log(response);
-      res.status(200).json({src: req.file.location, stats: response})
+      res.status(200).json({ src: req.file.location, stats: response });
     }
   });
-})
+});
 
 
-app.listen( 3000, () => {
-  console.log(`Listening on port ${ PORT }`);
-})
+app.listen(3000, () => {
+  console.log(`Listening on port ${PORT}`);
+});
